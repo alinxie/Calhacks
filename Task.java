3f709caeae1b36f0960821393e4b71361de1f707
kevinchen297 <kevinchen297@berkeley.edu>
2015-10-10 14:48:09 -0700

    Create Task.java
    Needs to be finished

---
+++ Task.java
@@ -0,0 +1,15 @@
+import java.util.*
+
+public class Task {
+  private String name;
+  private String description;
+  private int urgency;
+  private Date deadline;
+  
+  public Task() {
+    name = "";
+    description = "";
+    urgency = 0;
+    deadline = null;
+  }
+}
